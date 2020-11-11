f5dbc4219e837798fe483d90e107ec25917e0a9a
Kenneth Folke Larsen <KennethLarsenBusiness@hotmail.com>
2020-11-11 10:18:45 +0100

    Add project files.

---
+++ BlackPete/CardFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackPete
+{
+    public class CardFactory
+    {
+        private static CardFactory instance;
+        public static CardFactory Instace
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new CardFactory();
+                }
+                return instance;
+            }
+        }
+    }
+}
---
+++ BlackPete/PlayingCard.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackPete
+{
+    public enum Suit
+    {
+        Hearts,
+        Diamonds,
+        Spades,
+        Clubs
+    }
+    public class PlayingCard : Card
+    {
+        private Suit cardSuit;
+
+        public Suit CardSuit
+        {
+            get { return cardSuit; }
+            set { cardSuit = value; }
+        }
+
+        public PlayingCard(Suit cSuit,string name) : base(name)
+        {
+            CardSuit = cSuit;
+        }
+    }
+}
